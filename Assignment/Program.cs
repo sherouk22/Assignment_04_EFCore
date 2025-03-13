b400e8d0c49c81c40d844d9f8c4c5e10a11f0a41
sherouk22 <sheroukmohy558@gmail.com>
2025-03-14 00:58:31 +0200

    Assignment_04_EFCore

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace Assignment
+﻿using Assignment.Contexts;
+
+namespace Assignment
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+           using (NorthwindDbContext context = new NorthwindDbContext())
+            {
+
+                NorthwindDbContextProcedures contextProcedures = new NorthwindDbContextProcedures(context);
+
+                var Procedures = contextProcedures.SalesByCategoryAsync("Beverages","1998").Result;
+
+                foreach (var Procedure in Procedures)
+                    Console.WriteLine($"{Procedure.ProductName} :: {Procedure.TotalPurchase}");
+               
+
+
+            }
         }
     }
 }
