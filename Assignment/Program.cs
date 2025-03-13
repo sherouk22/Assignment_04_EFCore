a0e931bcd53802c2301851f7368825deaba5af49
sherouk22 <sheroukmohy558@gmail.com>
2025-03-14 01:57:15 +0200

    Assignment_04_EFCore

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment.Contexts;
+using Microsoft.EntityFrameworkCore;
 
 namespace Assignment
 {
@@ -6,19 +7,47 @@ namespace Assignment
     {
         static void Main(string[] args)
         {
-           using (NorthwindDbContext context = new NorthwindDbContext())
+
+           
+
+            using (NorthwindDbContext context = new NorthwindDbContext())
             {
+                #region Stored Procedure
 
-                NorthwindDbContextProcedures contextProcedures = new NorthwindDbContextProcedures(context);
+                //NorthwindDbContextProcedures contextProcedures = new NorthwindDbContextProcedures(context);
 
-                var Procedures = contextProcedures.SalesByCategoryAsync("Beverages","1998").Result;
+                //var Procedures = contextProcedures.SalesByCategoryAsync("Beverages", "1998").Result;
 
-                foreach (var Procedure in Procedures)
-                    Console.WriteLine($"{Procedure.ProductName} :: {Procedure.TotalPurchase}");
-               
+                //foreach (var Procedure in Procedures)
+                //    Console.WriteLine($"{Procedure.ProductName} :: {Procedure.TotalPurchase}");
+
+                #endregion
+
+
+                #region Run Sql Query 
+                // 1. Execute Select Statement
+
+                //var Result = context.Categories.FromSqlRaw("select * from Categories ").ToList();
+                // var Result = context.Categories.FromSqlInterpolated($"select * from Categories ").ToList();
+
+                //var Result = context.Categories.Where(x => x.CategoryName == "Beverages");
+
+                //foreach (var item  in Result) 
+                //    Console.WriteLine($" {item.CategoryName} :: {item.Description}");
+
+
+                // 2. Execute DML Statement
+
+                context.Database.ExecuteSqlInterpolated($"update Products set ProductName = 'Chai02' where ProductId ={1}" );
+                #endregion
+
 
 
             }
+
+
+
+
         }
     }
 }
